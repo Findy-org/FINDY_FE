0751f64faff4afbd1a4c0284bd7ceea7f6457668
keemsebeen <sebin0580@gmail.com>
2024-10-12 04:21:52 +0900

    chore : axios setting

---
+++ src/lib/axios.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+import { ENV } from './env';
+
+export const instance = axios.create({
+  baseURL: ENV.API_BASE_URL,
+  timeout: 5000,
+});
+
+export const get = <T>(...args: Parameters<typeof instance.get>) => {
+  return instance.get<T, T>(...args);
+};
+
+export const post = <T>(...args: Parameters<typeof instance.post>) => {
+  return instance.post<T, T>(...args);
+};
+
+export const put = <T>(...args: Parameters<typeof instance.put>) => {
+  return instance.put<T, T>(...args);
+};
+
+export const patch = <T>(...args: Parameters<typeof instance.patch>) => {
+  return instance.patch<T, T>(...args);
+};
+
+export const del = <T>(...args: Parameters<typeof instance.delete>) => {
+  return instance.delete<T, T>(...args);
+};
