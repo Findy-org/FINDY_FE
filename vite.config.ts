cceca2d1bd862ab6eff7ecd5712c60c20b0c4733
keemsebeen <sebin0580@gmail.com>
2024-10-22 04:49:46 +0900

    chore : edit config for svgr

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
+// https://vitejs.dev/config/
 // eslint-disable-next-line no-restricted-exports
 export default defineConfig({
   plugins: [svgr(), react()],
