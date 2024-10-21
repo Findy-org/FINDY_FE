2c5bd9bc971d415c0557db80a5ae35768605038e
keemsebeen <sebin0580@gmail.com>
2024-10-22 00:20:09 +0900

    fix : add color to tailwind config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
+// eslint-disable-next-line no-restricted-exports
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
