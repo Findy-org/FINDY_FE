a1a25261637d1de523b59b8eb57785d4b7767a6c
keemsebeen <sebin0580@gmail.com>
2024-10-22 23:39:52 +0900

    feat : add queryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App.tsx';
@@ -8,8 +9,20 @@ if (import.meta.env.DEV) {
   const { worker } = await import('./mocks/browser.ts');
   worker.start();
 }
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
