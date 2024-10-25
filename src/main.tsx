752073259a05b29dcde358804707a81dfb08bc09
keemsebeen <sebin0580@gmail.com>
2024-10-26 00:59:21 +0900

    feat : configure RouterProvider with router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createRoot } from 'react-dom/client';
-
-import { App } from './App.tsx';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './routes/route.tsx';
 
 if (import.meta.env.DEV) {
   const { worker } = await import('./mocks/browser.ts');
@@ -19,10 +20,10 @@ const queryClient = new QueryClient({
   },
 });
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import { App } from '@/App';
+import { Map } from '@/pages/Map';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [{ path: 'map', element: <Map /> }],
   },
 ]);
