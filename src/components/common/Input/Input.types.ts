2f2ed8cf19b1cfbcc0fc3967a6bffc58af922fd6
keemsebeen <sebin0580@gmail.com>
2024-10-30 15:38:52 +0900

    feat: add logic to set colorValue based on gray shades

--- src/components/common/Input/Input.types.ts
+++ src/components/common/Input/Input.types.ts
@@ -1,19 +1,20 @@
 import { ComponentPropsWithoutRef } from 'react';
 
 export type Props = {
-  /*
+  /**
    * The current value of the input field.
    */
   value: string;
-  /*
+  /**
    * Function to reset the input value to its initial state.
    */
   onClickReset: VoidFunction;
-  /*
+  /**
    * Indicates whether the current input value is valid.
+   * @default false
    */
-  isValid: boolean;
-  /*
+  isValid?: boolean;
+  /**
    * Optional error message to display when validation fails.
    */
   errorMessage?: string;
