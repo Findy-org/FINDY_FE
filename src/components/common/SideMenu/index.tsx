782989aef52ad645cd742586bc0191c8b11a804c
keemsebeen <sebin0580@gmail.com>
2024-10-30 15:59:45 +0900

    feat: implement SideMenu component with tooltip and variant

---
+++ src/components/common/SideMenu/index.tsx
@@ -0,0 +1,35 @@
+import { sideMenuTooltip } from '@/constants/sideMenuItem';
+
+import { GroupProps, Props } from './SideMenu.types';
+
+import { cn } from '../../../lib/core';
+import { IconButton } from '../IconButton';
+import { Tooltip } from '../Tooltip';
+
+export const SideMenu = ({ variant, position = 'left', className = '', ...props }: Props) => {
+  return (
+    <button className="relative" {...props}>
+      <IconButton name={variant} className={cn('peer', className)} />
+      <Tooltip
+        message={sideMenuTooltip[variant]}
+        position={position}
+        className="invisible opacity-0 transition-all duration-200 [@media(hover:hover)]:peer-hover:visible [@media(hover:hover)]:peer-hover:opacity-100"
+      />
+    </button>
+  );
+};
+
+const SideMenuGroup = ({ position = 'left', children, ...props }: GroupProps) => {
+  return (
+    <div
+      className={cn(
+        `w-fit wrap flex flex-col gap-4 ${position === 'left' ? 'left-4' : 'right-4'}         `
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+SideMenu.Group = SideMenuGroup;
