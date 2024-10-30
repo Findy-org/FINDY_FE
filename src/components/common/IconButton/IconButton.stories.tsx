3560e3ec6797fc29bae4557712ad25d541648662
keemsebeen <sebin0580@gmail.com>
2024-10-30 15:40:38 +0900

    feat: set up Storybook for IconButton component

---
+++ src/components/common/IconButton/IconButton.stories.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-restricted-exports */
+import { useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { IconButton } from '.';
+
+const meta = {
+  title: 'components/common/IconButton',
+  component: IconButton,
+  tags: ['autodocs'],
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof IconButton>;
+
+export const Basic: Story = {
+  render: () => {
+    const [value, setValue] = useState(false);
+    return (
+      <div className="flex flex-row flex-wrap gap-2">
+        <IconButton name="map" />
+        <IconButton name="link" />
+        <IconButton
+          name="bookMark"
+          variant="bookmark"
+          isActive={value}
+          onClick={() => setValue(!value)}
+        />
+      </div>
+    );
+  },
+};
+
+export const BookMark: Story = {
+  render: () => {
+    const [value, setValue] = useState(false);
+    return (
+      <div className="flex flex-row gap-4">
+        <IconButton
+          variant="bookmark"
+          name="bookMark"
+          isActive={value}
+          onClick={() => setValue(!value)}
+        />
+      </div>
+    );
+  },
+};
