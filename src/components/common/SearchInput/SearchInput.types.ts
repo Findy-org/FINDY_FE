858ccacba03f40dbd57a0349c8fd022b1a0896da
keemsebeen <sebin0580@gmail.com>
2024-11-04 21:57:05 +0900

    feat: Change Icon tag to button in SearchInput

--- src/components/common/SearchInput/SearchInput.types.ts
+++ src/components/common/SearchInput/SearchInput.types.ts
@@ -5,4 +5,8 @@ export type Props = {
    * The current value of the searchInput field.
    */
   value: string;
+  /**
+   * Function called when the search button is clicked.
+   */
+  onClick: () => void;
 } & ComponentPropsWithoutRef<'input'>;
--- src/components/common/SearchInput/index.tsx
+++ src/components/common/SearchInput/index.tsx
@@ -16,12 +16,13 @@ export const SearchInput = forwardRef<HTMLInputElement, Props>(
             className="w-full h-11 text-body3 bg-white pl-5 pr-10 rounded-lg border focus:outline-none border-primary transition-colors"
             {...props}
           />
-          <div
+          <button
+            type="button"
             className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
             onClick={onClick}
           >
             <Icon name="search" size={20} />
-          </div>
+          </button>
         </div>
       </div>
     );
