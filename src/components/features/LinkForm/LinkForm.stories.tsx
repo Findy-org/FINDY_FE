8ed2b18446bcaeb248d074430f5acfaf559baa3a
keemsebeen <sebin0580@gmail.com>
2024-11-05 17:09:27 +0900

    feat : set up Storybook for the Link Input component

---
+++ src/components/features/LinkForm/LinkForm.stories.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable no-restricted-exports */
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Layout } from '@/components/common/Layout';
+import { linkStepNames } from '@/constants/linkForm';
+import { useFunnel } from '@/hooks/common/useFunnel';
+
+import { ExtractionStatus, LinkInput, Landing } from '.';
+
+const meta = {
+  title: 'components/feature/LinkForm',
+  component: Landing,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Landing>;
+
+export default meta;
+
+type Story = StoryObj<typeof Landing>;
+
+export const Basic: Story = {
+  render: () => {
+    const { Funnel, Step, setStep } = useFunnel(linkStepNames[0]);
+
+    return (
+      <Layout>
+        <Funnel>
+          <Step name="입력전">
+            <Landing onNext={() => setStep('링크입력')} onHomeClick={() => {}} />
+          </Step>
+
+          <Step name="링크입력">
+            <LinkInput onNext={() => setStep('추출상태')} onHomeClick={() => {}} />
+          </Step>
+
+          <Step name="추출상태">
+            <ExtractionStatus onNext={() => {}} isLoading={true} onHomeClick={() => {}} />
+          </Step>
+        </Funnel>
+      </Layout>
+    );
+  },
+};
