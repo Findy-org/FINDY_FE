e2215bd0b7979b75d9dad6d92f15b5676847ab5b
keemsebeen <sebin0580@gmail.com>
2024-11-05 17:12:59 +0900

    feat : define types for LinkForm

---
+++ src/components/features/LinkForm/types.ts
@@ -0,0 +1,10 @@
+export type LinkFormProps = {
+  /**
+   * Function to be called when moving to the next step.
+   */
+  onNext: () => void;
+  /**
+   * Function to be called when clicking the home button.
+   */
+  onHomeClick: () => void;
+};
