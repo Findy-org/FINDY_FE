bee78c08c599eedc4b55d7f322e43590c6e20071
keemsebeen <sebin0580@gmail.com>
2024-11-09 23:05:24 +0900

    fix : define categoryType

--- src/utils/parseCategory.ts
+++ src/utils/parseCategory.ts
@@ -1,4 +1,14 @@
-const CATEGORY_REGEX_MAPPING: { [key: string]: RegExp } = {
+type CategoryType =
+  | 'restaurant'
+  | 'cafe'
+  | 'bar'
+  | 'shopping'
+  | 'public'
+  | 'travel'
+  | 'hospital'
+  | 'other';
+
+const CATEGORY_REGEX_MAPPING: Record<Exclude<CategoryType, 'other'>, RegExp> = {
   restaurant: /음식점|한식|중식|양식|일식/,
   cafe: /카페|디저트/,
   bar: /술집/,
@@ -6,7 +16,7 @@ const CATEGORY_REGEX_MAPPING: { [key: string]: RegExp } = {
   public: /공공|교육|사회/,
   travel: /명소|여행/,
   hospital: /병원|의원/,
-};
+} as const;
 
 export const parseCategory = (categoryName: string): string => {
   for (const [category, regex] of Object.entries(CATEGORY_REGEX_MAPPING)) {
