22532369cf574894f537f66cbb6afa6930911734
keemsebeen <sebin0580@gmail.com>
2024-11-09 17:41:00 +0900

    feat : implement for category filtering

---
+++ src/utils/parseCategory.ts
@@ -0,0 +1,25 @@
+export const parseCategory = (categoryName: string) => {
+  if (categoryName.includes('식')) {
+    return 'restaurant';
+  }
+  if (categoryName.includes('카페') || categoryName.includes('디저트')) {
+    return 'cafe-dessert';
+  }
+  if (categoryName.includes('술집')) {
+    return 'bar';
+  }
+  if (categoryName.includes('쇼핑') || categoryName.includes('유통')) {
+    return 'shopping';
+  }
+  if (categoryName.includes('여행') || categoryName.includes('명소')) {
+    return 'travel';
+  }
+  if (categoryName.includes('공공') || categoryName.includes('사회')) {
+    return 'public';
+  }
+  if (categoryName.includes('병원') || categoryName.includes('의원')) {
+    return 'hospital';
+  }
+
+  return 'other';
+};
