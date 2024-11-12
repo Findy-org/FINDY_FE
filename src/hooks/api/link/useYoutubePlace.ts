62f478c173ee21ec84f452fe426317cb85efdd6b
keemsebeen <sebin0580@gmail.com>
2024-11-12 23:17:44 +0900

    feat: implement useYoutubePlace hook for data extraction

---
+++ src/hooks/api/link/useYoutubePlace.ts
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { get } from '@/lib/external';
+import { Place } from '@/types/naver';
+
+export type YoutubeResponse = {
+  youtuberId: string;
+  youtuberName: string;
+  youtuberProfile: string;
+  youtubeLink: string;
+  places: Place[];
+};
+
+export const useYoutubePlace = (youtubeLink: string) => {
+  return useQuery<YoutubeResponse>({
+    queryKey: ['youtubeLink', youtubeLink],
+    queryFn: () => get<YoutubeResponse>(`video/place/${youtubeLink}`),
+    enabled: !!youtubeLink,
+  });
+};
