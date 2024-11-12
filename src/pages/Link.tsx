97add06f9c0567076c3046af1a59b89d4b8a7239
keemsebeen <sebin0580@gmail.com>
2024-11-12 23:19:40 +0900

    feat: connect API to Link page

--- src/pages/Link.tsx
+++ src/pages/Link.tsx
@@ -3,15 +3,30 @@ import { useNavigate } from 'react-router-dom';
 
 import { ExtractionStatus, Landing, LinkInput } from '@/components/features/LinkForm';
 import { linkStepNames } from '@/constants/linkForm';
+import { useYoutubePlace } from '@/hooks/api/link/useYoutubePlace';
 import { useFunnel } from '@/hooks/common/useFunnel';
+import { useYoutubeContext } from '@/hooks/common/useYoutubeContext';
+import { Place } from '@/types/naver';
 
 export const Link = () => {
-  const { Funnel, Step, setStep } = useFunnel(linkStepNames[0]);
+  const [link, dispatch] = useYoutubeContext();
   const navigate = useNavigate();
+  const { Funnel, Step, setStep } = useFunnel(linkStepNames[0]);
 
   const handleHomeClick = useCallback(() => {
     navigate('/map');
   }, [navigate]);
+
+  const {
+    refetch: fetchPlaces,
+    data,
+    isLoading,
+  } = useYoutubePlace(encodeURIComponent(link.youtubeLink));
+
+  const handleLinkSubmit = useCallback(() => {
+    fetchPlaces();
+  }, [fetchPlaces]);
+
   return (
     <Funnel>
       <Step name="입력전">
@@ -19,11 +34,30 @@ export const Link = () => {
       </Step>
 
       <Step name="링크입력">
-        <LinkInput onNext={() => setStep('추출상태')} onHomeClick={handleHomeClick} />
+        <LinkInput
+          onNext={() => {
+            handleLinkSubmit();
+            setStep('추출상태');
+          }}
+          onHomeClick={handleHomeClick}
+          context={{
+            state: link.youtubeLink,
+            setState: (link) => {
+              dispatch({ type: 'link', payload: { youtubeLink: link } });
+            },
+          }}
+        />
       </Step>
 
       <Step name="추출상태">
-        <ExtractionStatus onNext={() => {}} isLoading={true} onHomeClick={handleHomeClick} />
+        <ExtractionStatus
+          url={link.youtubeLink}
+          place={data?.places as Place[]}
+          onPrev={() => setStep('링크입력')}
+          onNext={() => navigate('/map', { state: { data } })}
+          isLoading={isLoading}
+          onHomeClick={handleHomeClick}
+        />
       </Step>
     </Funnel>
   );
