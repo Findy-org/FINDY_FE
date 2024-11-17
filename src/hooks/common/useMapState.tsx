ba2586ef680815a90b8c1bc9c7eceac4c0f19bd9
keemsebeen <sebin0580@gmail.com>
2024-11-17 23:21:14 +0900

    fix : modify useMapState type

--- src/hooks/common/useMapState.tsx
+++ src/hooks/common/useMapState.tsx
@@ -1,9 +1,14 @@
 import { useState, useCallback } from 'react';
 
+import { Coordinates } from '@/components/features/NaverMap/NaverMap.types';
+
 export const useMapState = (initialLat = 37.549681, initialLng = 126.991911, initialZoom = 13) => {
-  const [initialCenter, setInitialCenter] = useState({ lat: initialLat, lng: initialLng });
-  const [zoomLevel, setZoomLevel] = useState(initialZoom);
-  const [isCurrent, setIsCurrent] = useState(false);
+  const [initialCenter, setInitialCenter] = useState<Coordinates>({
+    lat: initialLat,
+    lng: initialLng,
+  });
+  const [zoomLevel, setZoomLevel] = useState<number>(initialZoom);
+  const [isCurrent, setIsCurrent] = useState<boolean>(false);
 
   const updateLocation = useCallback((lat: number, lng: number, zoom: number) => {
     setInitialCenter({ lat, lng });
