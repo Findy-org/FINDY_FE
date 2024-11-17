8323f829b4ee352e6565afe9393e809ff875d1b1
keemsebeen <sebin0580@gmail.com>
2024-11-17 23:49:49 +0900

    style : apply min-width

--- src/components/common/Chip/Chip.variants.ts
+++ src/components/common/Chip/Chip.variants.ts
@@ -3,9 +3,9 @@ import { cva } from 'class-variance-authority';
 export const ChipVariants = cva(`bg-primary rounded-md text-white cursor-default px-3 py-1`, {
   variants: {
     variant: {
-      small: 'h-6 w-6 text-caption',
-      medium: 'h-7 w-7 text-body4',
-      large: 'h-8 w-8 text-body3',
+      small: 'h-6 min-w-6 text-caption',
+      medium: 'h-7 min-w-7 text-body4',
+      large: 'h-8 min-w-8 text-body3',
     },
   },
   defaultVariants: {
