8b9507c2c5f43034b078c4b50d1603df3bc1deb7
keemsebeen <sebin0580@gmail.com>
2024-11-19 01:43:09 +0900

    style: update SideMenu and ListCard css, add check icon

--- src/components/common/ListCard/index.tsx
+++ src/components/common/ListCard/index.tsx
@@ -4,7 +4,7 @@ import { Props } from './ListCard.types';
 
 export const ListCard = forwardRef<HTMLDivElement, Props>(({ children, ...props }, ref) => {
   return (
-    <div className="w-full h-fit py-6 pl-6 pr-3.5 rounded-2xl bg-gray-100" {...props} ref={ref}>
+    <div className="w-full h-fit py-5 pl-6 pr-5 rounded-2xl bg-gray-50" {...props} ref={ref}>
       {children}
     </div>
   );
