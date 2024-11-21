c130ab951f1e75632124cedb81f857f9fe8bfb94
keemsebeen <sebin0580@gmail.com>
2024-11-21 23:36:14 +0900

    chore : remove provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
 import { Layout } from './components/common/Layout';
-import { MapDataProvider } from './contexts/MapContext';
-import { MarkerProvider } from './contexts/MarkerContext';
 import { useAuth } from './hooks/auth/useAuth';
 
 export const App = () => {
   useAuth();
   return (
-    <MapDataProvider>
-      <MarkerProvider>
-        <Layout>
-          <Outlet />
-        </Layout>
-      </MarkerProvider>
-    </MapDataProvider>
+    <Layout>
+      <Outlet />
+    </Layout>
   );
 };
