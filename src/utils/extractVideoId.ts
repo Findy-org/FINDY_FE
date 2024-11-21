8e1402ab627f1d60931d5d51c580e4494d9ce6a4
keemsebeen <sebin0580@gmail.com>
2024-11-21 23:52:08 +0900

    fix: separate YouTube thumbnail extraction function

---
+++ src/utils/extractVideoId.ts
@@ -0,0 +1,6 @@
+export const extractVideoId = (url: string): string | null => {
+  const match = url.match(
+    /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/\s]{11})/
+  );
+  return match ? match[1] : null;
+};
