025357614c56ff38d9bfc29dcb74baedcec03613
keemsebeen <sebin0580@gmail.com>
2024-11-22 01:22:28 +0900

    feat: implement BookmarkList component

--- src/components/features/BookmarkList/BookmarkList.tsx
+++ src/components/features/BookmarkList/BookmarkList.tsx
@@ -1,10 +1,53 @@
+import { Icon } from '@/components/common/Icon';
 import { ListCard } from '@/components/common/ListCard';
+import { Body1, Body2, Body3 } from '@/components/common/Typography';
+import { useBookMarkList } from '@/hooks/api/bookmarks/useBookMarkList';
+import { useAuth } from '@/hooks/auth/useAuth';
 
-export const BookmarkList = () => {
+type Props = { onNext: (bookmarkId: number) => void };
+export const BookmarkList = ({ onNext }: Props) => {
+  const { token } = useAuth();
+  const { data } = useBookMarkList(token);
+
+  const handleBookmarkClick = (bookmarkId: number) => {
+    onNext(bookmarkId);
+  };
   return (
     <div>
-      나의 핀디 리스트
-      <ListCard>리스트 조회</ListCard>
+      <Body1 className="my-4 mx-3" weight="semibold">
+        나의 핀디 리스트
+      </Body1>
+      <ListCard>
+        {data?.data.map((item, index) => (
+          <>
+            <div
+              key={item.bookmarkId}
+              onClick={() => handleBookmarkClick(item.bookmarkId)}
+              className="flex flex-row justify-between items-center cursor-pointer"
+            >
+              <div className="flex flex-row gap-4 py-2.5 items-center justify-center">
+                {item.youtuberProfile ? (
+                  <img
+                    src={item.youtuberProfile}
+                    className="w-12 h-12 rounded-full"
+                    alt={`${item.name}의 프로필 이미지`}
+                  />
+                ) : (
+                  <Icon name="findy1" className="w-11 h-11" />
+                )}
+                <div className="flex flex-col py-1">
+                  <Body2 weight="medium">{item.name}</Body2>
+                  <div className="flex flex-row items-center gap-1">
+                    <Icon name="location" size={15} />
+                    <Body3 className=" text-gray-500">{item.markersCount}</Body3>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {index < data.data.length - 1 && <hr className="border-dashed pt-2" />}
+          </>
+        ))}
+      </ListCard>
     </div>
   );
 };
