ec8d40ff802c1cc86b314bc065b02606b1ae9060
keemsebeen <sebin0580@gmail.com>
2024-11-23 23:34:58 +0900

    feat: add KeyDown event to handle Enter in SearchInput

--- src/components/common/SearchInput/index.tsx
+++ src/components/common/SearchInput/index.tsx
@@ -6,6 +6,12 @@ import { Icon } from '../Icon';
 
 export const SearchInput = forwardRef<HTMLInputElement, Props>(
   ({ value, onClick, disabled, ...props }, ref) => {
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter' && !disabled) {
+        onClick?.();
+      }
+    };
+
     return (
       <div className="flex flex-col items-start w-full">
         <div className="relative flex items-center w-full">
@@ -15,6 +21,7 @@ export const SearchInput = forwardRef<HTMLInputElement, Props>(
             placeholder="검색어를 입력해주세요."
             className="w-full h-11 text-body3 bg-white pl-5 pr-10 rounded-lg border focus:outline-none border-primary transition-colors"
             disabled={disabled}
+            onKeyDown={handleKeyDown}
             {...props}
           />
           <button
