08ba51e66466b123362cb8d3cead395dd75b40ed
houony <hoon7227@naver.com>
2024-11-25 22:45:33 +0900

    design : change width, remove margin

--- src/components/common/Dropdown/index.tsx
+++ src/components/common/Dropdown/index.tsx
@@ -15,15 +15,13 @@ export const Dropdown = ({ selectedCategory, onSelectCategory, className }: Prop
         className={DropdownVariants({ state: 'default' })}
         onClick={handleToggle}
       >
-        <div className="flex items-center">
-          {selectedCategory}
-          <span className="absolute ml-14">
-            <Icon name={isOpen ? 'arrowUp' : 'arrowDown'} size={10} />
-          </span>
+        <div className="flex items-center justify-between w-16">
+          <span>{selectedCategory}</span>
+          <Icon name={isOpen ? 'arrowUp' : 'arrowDown'} size={10} />
         </div>
       </button>
       {isOpen && (
-        <div className="absolute bg-white w-20 shadow-lg rounded-lg mt-0 z-modal max-h-[10.6rem] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-gray-300 [&::-webkit-scrollbar-thumb]:rounded">
+        <div className="absolute bg-white w-[5.5rem] shadow-lg rounded-lg mt-0 z-modal max-h-[10.6rem] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-gray-300 [&::-webkit-scrollbar-thumb]:rounded">
           {categories.map((category) => (
             <button
               key={category.name}
