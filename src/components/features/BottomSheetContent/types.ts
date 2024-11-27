6a033cf3074b8d987ee52122e9c0b38fae60f0a2
keemsebeen <sebin0580@gmail.com>
2024-11-27 23:05:36 +0900

    fix: Update type to ExtractResponse

--- src/components/features/BottomSheetContent/types.ts
+++ src/components/features/BottomSheetContent/types.ts
@@ -1,7 +1,7 @@
-import { YoutubeResponse } from '@/hooks/api/link/useYoutubePlace';
+import { ExtractResponse } from '@/hooks/api/link/useYoutubePlace';
 import { Place } from '@/types/naver';
 
 export type BottomSheetContentProps = {
   type: 'search' | 'extract' | 'list' | null;
-  data?: Place[] | YoutubeResponse | null;
+  data?: Place[] | ExtractResponse | null;
 };
