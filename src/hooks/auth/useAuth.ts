c618a1ce13d71a3d8a8acc25ac3f7fc79b1d9e78
keemsebeen <sebin0580@gmail.com>
2024-11-30 03:58:35 +0900

    fix : remove token retrieval using search params

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,21 +1,9 @@
-import { useEffect } from 'react';
 import { useCookies } from 'react-cookie';
-import { useNavigate, useSearchParams } from 'react-router-dom';
 
 export const useAuth = () => {
-  const navigate = useNavigate();
   const [cookies, setCookie, removeCookie] = useCookies(['token']);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const token = cookies.token;
 
-  useEffect(() => {
-    const tokenFromURL = searchParams.get('token');
-    if (tokenFromURL) {
-      setCookie('token', tokenFromURL, { path: '/' });
-      setSearchParams({}, { replace: true });
-      navigate('/map', { replace: true });
-    }
-  }, [navigate, searchParams, setCookie, setSearchParams]);
+  const token = cookies.token;
 
   const removeToken = () => {
     removeCookie('token', { path: '/' });
