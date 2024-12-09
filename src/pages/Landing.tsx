3d09339b04855effa4feddb998b1f8f2a2e37171
keemsebeen <sebin0580@gmail.com>
2024-12-10 00:53:26 +0900

    style: adjust height value for Swiper container

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -15,26 +15,24 @@ export const Landing = () => {
   return (
     <>
       <div className="w-full flex flex-col items-center justify-between">
-        <div className="flex flex-col items-center justify-center gap-5 mt-20 xs:mt-28">
+        <div className="flex flex-col items-center justify-center gap-5 mt-20">
           <Icon name="findyLogo1" className="w-2/3 h-20" />
           <Body1>핀디와 함께, 특별한 순간을 찾아보세요</Body1>
         </div>
-        <Swiper
-          {...swiperOptions}
-          className="w-full h-80 xs:h-96 max-w-md px-4 my-10 rounded-lg shadow-lg"
-        >
-          {LANDING_IMAGES.map((item, index) => (
-            <SwiperSlide key={index} className="flex justify-center items-center ">
-              <img
-                src={`/landing/${item}.png`}
-                alt={`${item} 이미지`}
-                loading="lazy"
-                className="w-full h-full object-cover"
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
+        <div className="flex justify-center items-center w-full max-h-[calc(100dvh-250px)] mt-8 xs:mt-16">
+          <Swiper {...swiperOptions} className="w-full xs:h-96 max-w-md px-4 rounded-lg shadow-lg">
+            {LANDING_IMAGES.map((item, index) => (
+              <SwiperSlide key={index} className="flex justify-center items-center ">
+                <img
+                  src={`/landing/${item}.png`}
+                  alt={`${item} 이미지`}
+                  loading="lazy"
+                  className="w-full h-full object-cover"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
         <div className="absolute bottom-3 w-full max-w-[30rem] px-4 mb-5">
           <Button variant="primary" size="large" onClick={() => navigate('/map')}>
             시작하기
